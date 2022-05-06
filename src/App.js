c14a8854a7b6edc7e30baf5ba922f1fb54081476
romerolaia <romerocarollaia@gmail.com>
2022-05-06 18:09:32 +0200

    search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       hello
-      </header>
+      <div className="container">
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <small>
+        Coded by Laia Romero
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
