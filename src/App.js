81f564650de46004cd35f4cae9ae29ae87c0defd
romerolaia <romerocarollaia@gmail.com>
2022-05-06 19:53:39 +0200

    css added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
+        <header className="App-header">Dictionary</header>
       <main>
         <Dictionary />
       </main>
