fd6ec23a04d3b0cb5d6e28b87b71ac36dc6a9c4c
romerolaia <romerocarollaia@gmail.com>
2022-05-07 16:59:11 +0200

    fixed link for deployment

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function(props){
     return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank">Listen</a>
+            <a href={props.phonetic.audio} target="_blank" el="noreferrer">Listen</a>
             {props.phonetic.text}
         </div>
     )
