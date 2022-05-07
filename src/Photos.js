b8876f4ab67c7218914b91c6da5f336b55a4241a
romerolaia <romerocarollaia@gmail.com>
2022-05-07 19:59:06 +0200

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ if(props.photos){
                 return(
                     <div className="col-4" key={index}>
                         <a href={photo.src.original} target="_blank" rel="noreferrer">
-                    <img src={photo.src.landscape} className="img-fluid" alt=""/>
+                    <img src={photo.src.landscape} className="img-fluid" alt={photo.src.photographer}/>
                     </a>
                     </div>
                 )
